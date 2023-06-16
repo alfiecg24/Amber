ff7584341801e3fa90c70be9f4f355cf2643e57d
alfiecg24 <alfiecockell@outlook.com>
2023-06-16 17:22:21 +0100

    Initial Commit

---
+++ IPAInstaller/IPAInstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPAInstallerApp.swift
+//  IPAInstaller
+//
+//  Created by Alfie on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct IPAInstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
